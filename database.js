4f30cbd10afbe2257d405752828136c37e5365fa
QoidynBasy <c0mplexie1@gmail.com>
2023-12-22 22:06:38 +0600

    bdsm

---
+++ database.js
@@ -0,0 +1,115 @@
+const {Client} = require('pg');
+
+class Database {
+    constructor() {
+        this.client = new Client({
+            postgresSQLLoginPassword: 'jdbc:postgresql://localhost:5432/postgres',
+        });
+    }
+    async connect() {
+        try {
+            await this.client.connect();
+            console.log('ЕСТЬ КОНТАК!!!');
+        } catch (err) {
+            console.error('Error connecting to PostgresSQL:', err);
+        } finally {
+            await this.client.end();
+            console.log('Connection closed!');
+        }
+    }
+
+    async createTable() {
+        const query = '' +
+            '   CREATE TABLE IF NOT EXISTS Квартира (\n' +
+            '      здание varchar(200) NOT NULL,\n' +
+            '      подъезд int NOT NULL,\n' +
+            '      этаж int NOT NULL,\n' +
+            '      квартира int NOT NULL,\n' +
+            '      квадратура float NOT NULL,\n' +
+            '      комнаты int NOT NULL,\n' +
+            '      стоимость int NOT NULL\n' +
+            '   );';
+        await this.client.query(query);
+        console.log('');
+    }
+
+    async insertData(house, entrance, floor, apartment, quadrature, rooms, cost) {
+        const query = '' +
+            'INSERT INTO Квартира (здание, подъезд, этаж, квартира, квадратура, комнаты, стоимость)\n' +
+            'VALUES\n' +
+            `('${house}', ${entrance}, ${floor}, ${apartment}, ${quadrature}, ${rooms}, ${cost}),`;
+        await this.client.query(query);
+        console.log('Квартира загружена');
+    }
+
+    async getDataByFloor(floor) {
+        const query = 'select квартира\n' +
+            'from "Квартиры"\n' +
+            `where этаж = ${floor}` +
+            'group by квартира;';
+        const result = await this.client.query(query);
+        console.log('Выбранные Данные:', result.rows);
+    }
+    async getDataByApartment(apartment) {
+        const query = 'select квартира\n' +
+            'from "Квартиры"\n' +
+            `where квартира = ${apartment}` +
+            'group by квартира;';
+        const result = await this.client.query(query);
+        console.log('Выбранные Данные:', result.rows);
+    }
+    async getDataByQuadratureMore(quadrature) {
+        const query = 'select квартира\n' +
+            'from "Квартиры"\n' +
+            `where квадратура > ${quadrature}` +
+            'group by квартира;';
+        const result = await this.client.query(query);
+        console.log('Выбранные Данные:', result.rows);
+    }
+    async getDataByQuadratureLess(quadrature) {
+        const query = 'select квартира\n' +
+            'from "Квартиры"\n' +
+            `where квартира < ${quadrature}` +
+            'group by квартира;';
+        const result = await this.client.query(query);
+        console.log('Выбранные Данные:', result.rows);
+    }
+    async getDataByEntrance(entrance) {
+        const query = 'select квартира\n' +
+            'from "Квартиры"\n' +
+            `where подъезд = ${entrance}` +
+            'group by квартира;';
+        const result = await this.client.query(query);
+        console.log('Выбранные Данные:', result.rows);
+    }
+    async getDataByRooms(rooms) {
+        const query = 'select квартира\n' +
+            'from "Квартиры"\n' +
+            `where комнаты = ${rooms}` +
+            'group by квартира;';
+        const result = await this.client.query(query);
+        console.log('Выбранные Данные:', result.rows);
+    }
+    async getDataByCostMore(cost) {
+        const query = 'select квартира\n' +
+            'from "Квартиры"\n' +
+            `where стоимость > ${cost}` +
+            'group by квартира;';
+        const result = await this.client.query(query);
+        console.log('Выбранные Данные:', result.rows);
+    }
+    async getDataByCostLess(cost) {
+        const query = 'select квартира\n' +
+            'from "Квартиры"\n' +
+            `where стоимость < ${cost}` +
+            'group by квартира;';
+        const result = await this.client.query(query);
+        console.log('Выбранные Данные:', result.rows);
+    }
+    async disconnect() {
+        await this.client.end();
+        console.log('База данных отсоединена');
+    }
+}
+
+module.exports = Database;
--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const Database = require("./database");
+
 const app = express();
 const port = 1337;
+const db = new Database();
+
+app.use(express.json());
 
 app.get('/', (req,res) => {
     res.send('Hello');
 });
 
+app.get('database/:apartmentName', async (req,res) => {
+    const { apartmentName } = req.params;
+
+    try {
+        await db.connect();
+        const apartmentInfo = await db.getDataBy(apartmentName);
+
+        if (apartmentInfo) {
+            res.json(apartmentInfo);
+        } else {
+            res.status(404).json({ message: 'Квартира не найдена'});
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Ошибка подключения к серверу'});
+    } finally {
+        await db.disconnect();
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
+
+db.createTable();
